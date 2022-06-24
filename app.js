ac23d6603bc1c3a34195aae9ed2b0cb3dbd319a2
Suzanne Mozes <suzanne.mozes@gmail.com>
2022-06-24 13:38:45 -0400

    added alien and math.random

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-testing 
+// hull=hitpoints. If hull <= 0, spaceship is destroyed.
+
+//firepower=amount of damage done to hull with a hit
+
+//accuracy=chance (0-1) that the ship will hit its target
+
 
 const spaceship{
-  hull: "gi20"
+  name: "USS HelloWorld"
+  hull: "20"
   firepower: "5"
   accuracy: "".7"
 }
 
+const alien{
+  constructor(
+  this.hull = hull
+  this.firepower = firepower
+  this.accuracy = accuracy
+  )
+}
+
+//example of use of accuracy to determine a hit:
+if(Math.random() < alien[0].accuracy) { 
+  console.log('You have been hit!');
+}
