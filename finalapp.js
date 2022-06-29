74cc64afe35ba0da3efc780a29081a13404b6b2a
Suzanne Mozes <suzanne.mozes@gmail.com>
2022-06-29 15:33:44 -0400

    recoded html into sbyt.html

--- finalapp.js
+++ finalapp.js
@@ -1,3 +1,21 @@
+//Defining ALL our global variables pointing to the different divs we need reference to
+
+
+//Define battle variables
+
+
+
+
+//call function when button is clicked
 function startGame(){
+
+}
+
+
+function attack(){
+
+}
+
+function alienAttack(){
   
 }
